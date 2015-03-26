96b5acf6e4e2b2373da3eb08df524685da21d1a7
Conrad VanLandingham <cv@utexas.edu>
2015-03-25 21:05:20 -0500

    remove empty test stubs and update test runner

--- config/environment.js
+++ config/environment.js
@@ -37,10 +37,10 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
 
     ENV.assetEndpoint = "";
     ENV.API_ENDPOINT = "//192.168.1.91:3000";
